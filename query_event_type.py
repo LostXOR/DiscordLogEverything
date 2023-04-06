bb97e5a27f04d6fac20ab5dc34996dfc304c3ba7
LostXOR <lostxor@gmail.com>
2023-04-06 17:59:56 -0500

    Fixed GitHub reporting project as mostly JavaScript when it's just... Not

--- query_event_type.py
+++ query_event_type.py
@@ -1,6 +1,7 @@
 # Just a debug program I use to query different event types in the database
 # Don't run this unless you're working on the code
+import sqlite3
 db = sqlite3.connect("database.db")
 cursor = db.cursor()
 while True:
-    print(cursor.execute(f'SELECT * FROM socket_recv WHERE json LIKE \'%"t":"'+input("event> ")+'"%\'').fetchone())
+    print(cursor.execute(f'SELECT * FROM socket_recv WHERE json LIKE \'%"t":"'+input("event> ")+'"%\'').fetchone()[2])
